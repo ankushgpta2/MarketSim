4ded25ea1038ae7beb83156d8050a183f34772dc
ankushkgupta2 <ankushkgupta@deloitte.com>
2024-06-12 18:48:55 -0400

    new script template for the actual AI model

---
+++ app/model.py
@@ -0,0 +1,7 @@
+"""
+Contains code for AI model handling the actual stock market learning. Incorporates timeseries, textual metadata for the stock, 
+and textual articles, tweets, etc. to find relevant news.
+
+Generation of artificial stock trends, company names, and news headlines / articles that make sense through this learning.
+"""
+
